c37e511e56184af8db740cbc6beb8354bd8b04ab
Jeedie JIDE <enockyona906@gmail.com>
2022-11-10 16:50:04 +0300

    append strikes

--- src/app/user/components/contact/contact.component.ts
+++ src/app/user/components/contact/contact.component.ts
@@ -1,40 +1,36 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import {NgForm} from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  filedata: any;
+  fileEvent(e: any) {
+    this.filedata = e.target.files[0];
   }
-
-  filedata:any;
-    fileEvent(e:any){
-        this.filedata = e.target.files[0];
-    }
-  constructor(private http: HttpClient) {
-  }
-   onSubmit(f: NgForm) {
-       
-            var myFormData = new FormData();
-            const headers = new HttpHeaders();
-          headers.append('Content-Type', 'multipart/form-data');
-          headers.append('Accept', 'application/json');
-          myFormData.append('image', this.filedata);
-        this.http.post('http://localhost/blog/public/api/sample-restful-apis', myFormData, {
-  headers: headers
-}).subscribe(data => {
-          console.log(data);
+  constructor(private http: HttpClient) {}
+  onSubmit(f: NgForm) {
+    var myFormData = new FormData();
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'multipart/form-data');
+    headers.append('Accept', 'application/json');
+    myFormData.append('image', this.filedata);
+    this.http
+      .post(
+        'http://localhost/blog/public/api/sample-restful-apis',
+        myFormData,
+        {
+          headers: headers,
+        }
+      )
+      .subscribe((data) => {
+        console.log(data);
       });
-        
-      }
-
-  
   }
-
- 
-
+}
