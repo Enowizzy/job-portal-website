a3b613789476854842afea49adf1c996b4a97ff4
Jeedie JIDE <enockyona906@gmail.com>
2022-11-11 20:28:36 +0300

    change API to addJobs

--- src/app/user/components/contact/contact.component.ts
+++ src/app/user/components/contact/contact.component.ts
@@ -25,13 +25,13 @@ export class ContactComponent implements OnInit {
     const headers = new HttpHeaders();
     headers.append('Content-Type', 'multipart/form-data');
     headers.append('Accept', 'application/json');
-    myFormData.append('image', this.filedata);
+    myFormData.append('pdf', this.filedata);
     myFormData.append('name', this.name);
     myFormData.append('email', this.email);
     myFormData.append('subject', this.subject);
     myFormData.append('message', this.message);
     this.http
-      .post('http://127.0.0.1:8000/api/addImages', myFormData, {
+      .post('http://127.0.0.1:8000/api/addJobs', myFormData, {
         headers: headers,
       })
       .subscribe((data) => {
