0a018c103d25d32a2b3326420ccf6b0556865a89
Jeedie JIDE <enockyona906@gmail.com>
2022-11-12 12:12:03 +0300

    alter job component

--- src/app/admin/components/job-post/job-post.component.ts
+++ src/app/admin/components/job-post/job-post.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
+import { Job } from 'src/app/models/job.model';;
 
 @Component({
   selector: 'app-job-post',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./job-post.component.css']
 })
 export class JobPostComponent implements OnInit {
-
-  constructor() { }
-
+  name: string = '';
+  email: string = '';
+  subject: string = '';
+  message: string = '';
+  job = new Job();
+  
   ngOnInit(): void {
+  }
+  filedata: any;
+  fileEvent(e: any) {
+    this.filedata = e.target.files[0];
+  }
+  constructor(private http: HttpClient) {}
+  onSubmit(f: NgForm) {
+    var myFormData = new FormData();
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'multipart/form-data');
+    headers.append('Accept', 'application/json');
+    myFormData.append('pdf', this.filedata);
+    myFormData.append('name', this.name);
+    myFormData.append('email', this.email);
+    myFormData.append('subject', this.subject);
+    myFormData.append('message', this.message);
+    this.http
+      .post('http://127.0.0.1:8000/api/addJobs', myFormData, {
+        headers: headers,
+      })
+      .subscribe((data) => {
+        console.log(data);
+      });
   }
 
 }
