2b8d027e6714bcd641a175c3388a6b2ad42cb268
Jeedie JIDE <enockyona906@gmail.com>
2022-11-21 10:51:14 +0300

    carousel options in jobs c

--- src/app/user/components/all-jobs/all-jobs.component.ts
+++ src/app/user/components/all-jobs/all-jobs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-all-jobs',
@@ -6,7 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-jobs.component.css']
 })
 export class AllJobsComponent implements OnInit {
-
+  imageDirectory:any =  environment.PUBLIC_URL;
+  getAllJobs: OwlOptions = {
+    loop: true,
+    autoplay: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: true,
+    navSpeed: 300,
+    navText: ['<', '>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 2
+      }
+    },
+    nav: true
+  }
   constructor() { }
 
   ngOnInit(): void {
